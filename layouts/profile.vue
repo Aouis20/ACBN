e7068f1606b0206705ecc37a316fe6fddca01cc5
Aouis CHOUKRI <78351892+Aouis20@users.noreply.github.com>
2024-06-09 18:26:22 +0200

    feat(Setup): :sparkles: App created

---
+++ layouts/profile.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex flex-col gap-2 mb-8">
+    <h1>Profile</h1>
+    <p>Bievenue sur votre profil, retrouvez ici toutes vos informations.</p>
+    <hr />
+  </div>
+  <slot />
+</template>
